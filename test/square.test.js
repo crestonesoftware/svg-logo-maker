59de699b97d3345c7bfb0ea4dbe4983c2e3e40c9
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-26 12:45:45 -0700

    Square constructor() and getData() working and tests passing

---
+++ test/square.test.js
@@ -0,0 +1,62 @@
+const { Square, Shape } = require("../lib/shapes");
+const fs = require("fs");
+
+describe("Shapes", () => {
+  describe("Squar", () => {
+    // tests constructor
+    it("constructor(x,y, width,height, fillColor)", () => {
+      const newSquare = new Square(50, 50, 100, 100, "green");
+      const expectedData =
+        "x: 50, y: 50, width: 100, height: 100, fillColor: green, xmlns: http://www.w3.org/2000/svg";
+      expect(newSquare.getData()).toEqual(expectedData);
+    });
+
+    // tests getData(). Is a copy of the constrctor test. 
+    // The duplication seems a little silly, but I'm just 
+    // iterating through a list of things to test!
+    it("getData()", () => {
+      const newSquare = new Square(50, 50, 100, 100, "green");
+      const expectedData =
+        "x: 50, y: 50, width: 100, height: 100, fillColor: green, xmlns: http://www.w3.org/2000/svg";
+      expect(newSquare.getData()).toEqual(expectedData);
+    });
+
+    // it("render()", () => {
+    //   const newShape = new Shape(50, 50, "blue");
+    //   expect(() => newShape.render()).toThrow(
+    //     "Call render() from a child class"
+    //   );
+    // });
+
+    // // it("writeToFile() - file exists", () => {
+    // //   const newShape = new Shape(50, 50, "blue");
+    // //   const filePath = "./tmp/newShape.html";
+    // //   newShape.writeToFile(filePath);
+    // //   expect(fs.existsSync(filePath));
+    // // });
+
+    // // it("writeToFile() - file contents written", () => {
+    // //   const newShape = new Shape(50, 50, "blue");
+    // //   const filePath = "./tmp/newShape.html";
+    // //   const fileContents = newShape.getData();
+    // //   newShape.writeToFile(filePath);
+    // //   const content = fs.readFileSync(filePath, "utf8");
+    // //   expect(content).toEqual(fileContents);
+    // // });
+
+    // it("writeToFile() - throws error when called from superclass", () => {
+    //   const newShape = new Shape(50, 50, "blue");
+    //   const filePath = "./tmp/newShape.html";
+
+    //   expect(() => newShape.writeToFile(filePath)).toThrow();
+    // });
+  });
+});
+
+// describe('suite', () => {
+//     describe('class', () => {
+//         it('method/test', ()=> {
+
+//         });
+//     });
+// });
