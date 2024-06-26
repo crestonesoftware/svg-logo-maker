ba4cc8c40a465a70448023832719ef8b9e88a01f
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-26 06:14:19 -0700

    tests constructor and getData()

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -2,10 +2,19 @@ const { Square, Shape } = require("../lib/shapes");
 
 describe("Shapes", () => {
   describe("Shape", () => {
-    it("render()", () => {
+    it("constructor(width,height, fillColor)", () => {
       let newShape = new Shape(5, 5, "red");
-      console.log(newShape);
-      expect(1 == 2);
+      const expectedData =
+        "width: 5, height: 5, fillColor: red, xmlns: http://www.w3.org/2000/svg";
+      console.log(newShape.getData());
+      expect(newShape.getData()).toEqual(expectedData);
+    });
+    it("getData()", () => {
+      let newShape = new Shape(50, 50, "blue");
+      const expectedData =
+        "width: 50, height: 50, fillColor: blue, xmlns: http://www.w3.org/2000/svg";
+      console.log(newShape.getData());
+      expect(newShape.getData()).toEqual(expectedData);
     });
   });
 });
