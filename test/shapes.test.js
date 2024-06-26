41b5aa251c5fa7ee25a4db67e54d71cef1dd4ad4
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-26 06:22:42 -0700

    tests writeToFile() - output file exists

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,25 +1,35 @@
 const { Square, Shape } = require("../lib/shapes");
+const fs = require("fs");
 
 describe("Shapes", () => {
   describe("Shape", () => {
     it("constructor(width,height, fillColor)", () => {
-      let newShape = new Shape(5, 5, "red");
+      const newShape = new Shape(5, 5, "red");
       const expectedData =
         "width: 5, height: 5, fillColor: red, xmlns: http://www.w3.org/2000/svg";
       console.log(newShape.getData());
       expect(newShape.getData()).toEqual(expectedData);
     });
+
     it("getData()", () => {
-      let newShape = new Shape(50, 50, "blue");
+      const newShape = new Shape(50, 50, "blue");
       const expectedData =
         "width: 50, height: 50, fillColor: blue, xmlns: http://www.w3.org/2000/svg";
       expect(newShape.getData()).toEqual(expectedData);
     });
+
     it("render()", () => {
-      let newShape = new Shape(50, 50, "blue");
+      const newShape = new Shape(50, 50, "blue");
       expect(() => newShape.render()).toThrow(
         "Call render() from a child class"
       );
+    });
+
+    it("writeToFile() - file exists", () => {
+      const newShape = new Shape(50, 50, "blue");
+      const filePath = "../tmp/newshape.html";
+      newShape.writeToFile(filePath);
+      expect(fs.existsSync(filePath));
     });
   });
 });
@@ -31,13 +41,3 @@ describe("Shapes", () => {
 //         });
 //     });
 // });
-
-// describe('BlogPost', () => {
-//   // Test to verify that the BlogPost class can be instantiated.
-//   describe('Instantiate', () => {
-//     it('should be an instance of BlogPost class', () => {
-//       const blogpost = new BlogPost();
-
-//       expect(blogpost).toBeInstanceOf(BlogPost);
-//     });
-//   });
