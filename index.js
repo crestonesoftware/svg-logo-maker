ae9139fd3ed488e12cee806051ff0fe638114044
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-26 17:49:06 -0700

    attempting to create square

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // DEPENDENCIES
 const inquirer = require("inquirer");
 const questions = require("./lib/inquirer-questions.js");
+const shapes = require("./lib/shapes.js");
+const constants = require("./lib/constants.js");
 
 // DATA
 
 // FUNCTIONS
 
 async function generateLogo() {
-  console.log("generateLogo");
   const answers = await inquirer.prompt(questions);
-  
+  const { shape, text, color } = answers;
+  let logo = "";
+
+  switch (shape) {
+    case "Square":
+      logo = new shapes.Square(100, 100, 100, 100, color);
+      logo.writeToFile(constants.LOGO_FILE);
+  }
 }
 
 const init = () => {
