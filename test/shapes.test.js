b95414cee7aeb2c51bfcc229cf2fc79cbd19a7ed
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-26 18:10:24 -0700

    separated svg container height/width from shape height/width

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,42 +1,36 @@
 const { Square, Shape } = require("../lib/shapes");
+const { MAGIC_SUFFIX } = require("../lib/constants.js");
 const fs = require("fs");
 
 describe("Shapes", () => {
   describe("Shape", () => {
-    it("constructor(width,height, fillColor)", () => {
-      const newShape = new Shape(5, 5, "red");
+    it("constructor and getData()", () => {
+      const newShape = new Shape("red");
       const expectedData =
-        "width: 5, height: 5, fillColor: red, xmlns: http://www.w3.org/2000/svg";
-      expect(newShape.getData()).toEqual(expectedData);
-    });
-
-    it("getData()", () => {
-      const newShape = new Shape(50, 50, "blue");
-      const expectedData =
-        "width: 50, height: 50, fillColor: blue, xmlns: http://www.w3.org/2000/svg";
+        "container width: 300, container height: 200, fillColor: red, xmlns: http://www.w3.org/2000/svg";
       expect(newShape.getData()).toEqual(expectedData);
     });
 
     it("render()", () => {
-      const newShape = new Shape(50, 50, "blue");
+      const newShape = new Shape("blue");
       expect(() => newShape.render()).toThrow(
         "Call render() from a child class"
       );
     });
 
     it("renderSVGContainer()", () => {
-      const newShape = new Shape(50, 50, "blue");
-      const expectedResult = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">foo</svg>`;
+      const newShape = new Shape("blue");
+      const expectedResult = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">foo</svg>${MAGIC_SUFFIX}`;
       expect(newShape.renderSVGContainer("foo")).toEqual(expectedResult);
     });
-    it("writeToFile() - throws error b/c render is called from superclass", () => {
-      const newShape = new Shape(50, 50, "blue");
-      const filePath = "./tmp/newShape.html";
+    // it("writeToFile() - throws error b/c render is called from superclass", () => {
+    //   const newShape = new Shape(50, 50, "blue");
+    //   const filePath = "./tmp/newShape.html";
 
-      expect(() => newShape.writeToFile(filePath)).toThrow(
-        "Call render() from a child class"
-      );
-    });
+    //   expect(() => newShape.writeToFile(filePath)).toThrow(
+    //     "Call render() from a child class"
+    //   );
+    // });
   });
   // it("writeToFile() - file exists", () => {
   //   const newShape = new Shape(50, 50, "blue");
